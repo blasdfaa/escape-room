ae566c7c38ae511e78d8604effd0cd6dbe9dcc9c
Viktor Zdorovcev <v.zdvacew@gmail.com>
2021-12-05 11:31:30 +0700

    fix: Исправлена конфигурация сервера

--- server/server.js
+++ server/server.js
@@ -2,13 +2,21 @@ const jsonServer = require('json-server');
 const { initDatabase } = require('./database/database');
 const { initCustomRoutes } = require('./routes/routes');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const server = jsonServer.create();
 const router = jsonServer.router(initDatabase());
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: './build',
+});
 
 server.use(middlewares);
+
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  }),
+);
 
 server.use(jsonServer.bodyParser);
 
@@ -16,6 +24,6 @@ initCustomRoutes(server);
 
 server.use(router);
 
-server.listen(process.env.PORT || PORT, () => {
+server.listen(PORT, () => {
   console.log('JSON Server is running');
 });
